7831fa558d87538c77488997476787e67b9b8575
Adir Noyman <ofahol2007@hotmail.com>
2025-01-15 21:21:58 +0200

    .

--- src/SearchService/controllers/SearchController.cs
+++ src/SearchService/controllers/SearchController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.controllers
 {
@@ -10,18 +11,51 @@ namespace SearchService.controllers
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
         {
-            var query = DB.PagedSearch<Item>();
-
-            query.Sort(x => x.Ascending(y => y.Make));
-
-            if (!string.IsNullOrEmpty(searchTerm))
+            // Create the paged search query object
+            var query = DB.PagedSearch<Item, Item>();
+           
+            // Search the items based on the search parameter SearchTerm
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
             {
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+            }
+            
+            // Order (sort) the items based on the search parameter
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(y => y.Make)),
+                "new" => query.Sort(x => x.Descending(y => y.CreatedAt)),
+                // The default sort order is by AuctionEnd time from the soonest to the latest                
+                _ => query.Sort(x => x.Ascending(y => y.AuctionEnd))
+            };
+
+            // Filter the items based on the search parameter
+            query = searchParams.FilterBy switch
+            {
+                // Get auctions that are already finished
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                // Get auctions that are still ongoing but will end in the next 6 hours
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+                // The default filter is to show all auctions that are still ongoing
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+
+            // Filter the items based on the search parameter Seller
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
             }
 
-            query.PageNumber(pageNumber).PageSize(pageSize);
+            // Filter the items based on the search parameter Winner
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+            // Page the search results => Set the page number and page size
+            query.PageNumber(searchParams.PageNumber).PageSize(searchParams.PageSize);
             var result = await query.ExecuteAsync();
 
             return Ok(new
