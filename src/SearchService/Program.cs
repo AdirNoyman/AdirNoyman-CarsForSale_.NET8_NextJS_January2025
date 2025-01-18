326c7e73e5f523f768ecd952ea6e4a96ea3551e1
Adir Noyman <ofahol2007@hotmail.com>
2025-01-18 14:12:31 +0200

    .

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -6,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionsServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionsServiceHttpClient>().AddPolicyHandler(GetRetryPolicy());
 
 var app = builder.Build();
 
@@ -14,15 +17,29 @@ var app = builder.Build();
 
 app.MapControllers();
 
-// Initialize the MongoDB database including seeding if the database is empty
-try
+// Register the application started event. This makes sure that even if the db initialization fails, the application will still start, because it will skip this step.
+
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception error)
-{
-    
-    Console.WriteLine("An error occurred while initializing the database 😫", error);
-}
+    // Initialize the MongoDB database including seeding if the database is empty
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("An error occurred while initializing the database 😫", error);
+    }
+});
 
 app.Run();
+
+// Handle transient HTTP failures
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        // Retry every 3 seconds until the call is successful
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+}
